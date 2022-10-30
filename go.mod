69f7d1f918c13fd7c8ad64e1a8504af88c8d62b4
Dev-Daljeet (Daljeet Singh) <daljeet@devdaljeet.com>
2022-10-30 16:46:10 -0400

    Add token methods

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module goclient
 go 1.18
 
 require (
+	github.com/gin-gonic/gin v1.8.1
 	github.com/ignite/cli v0.24.0
 	mitoblockchaindev v0.0.0-00010101000000-000000000000
 )
@@ -42,7 +43,6 @@ require (
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 
     "net/http"
-//	"errors"
 	"github.com/gin-gonic/gin"
 
     "context"
@@ -37,154 +36,82 @@ func CORSMiddleware() gin.HandlerFunc {
     }
 }
 
-
-
-// temp data for testing purposes only
-type book struct {
-	ID       string `json:"id"`
-	Title    string `json:"title"`
-	Author   string `json:"author"`
-	Quantity int    `json:"quantity"`
-}
-
-// temp data for testing purposes only
-var books = []book{
-	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
-	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
-	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
-}
-
+// Prefix to use for account addresses.
+    // The address prefix was assigned to the blog blockchain
+    // using the `--address-prefix` flag during scaffolding.
     var addressPrefix = "mito"
 
+    // Create a Cosmos client instance
     var cosmos, err = cosmosclient.New(
         context.Background(),
         cosmosclient.WithAddressPrefix(addressPrefix),
     )
 
-    // Account `alice` was initialized during `ignite chain serve`
+    // Account `bob` was initialized during `ignite chain serve`
     var accountName = "bob"
 
     // Get account from the keyring
     var account, accounterr = cosmos.Account(accountName)
 
-    var addr, addresserr = account.Address(addressPrefix)
+    var addr, addrerr = account.Address(addressPrefix)
+	
+	var queryClient = types.NewQueryClient(cosmos.Context())
 
-    // Instantiate a query client for the blockchain
-    var queryClient = types.NewQueryClient(cosmos.Context())
-
-
-
-func getBooks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, books)
-}
-
-func getUser(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, addr)
-}
-
-
-func getTokens(c *gin.Context) {
-
-    // Query the blockchain using the client's `DiscountTokens` method
-    // to get all tokens store all tokens in queryResp
-    queryResp, qerr := queryClient.DiscountTokens(context.Background(), &types.QueryDiscountTokensRequest{})
-    if qerr != nil {
-        log.Fatal(err)
-    }
-
-    // Print response from querying all the tokens
-    fmt.Print("\n\nAll tokens:\n\n")
-    // fmt.Println(queryResp)
-
-    fmt.Print((queryResp.GetDiscountToken))
-	c.IndentedJSON(http.StatusOK, queryResp)
-}
-
-// TODO: test endpoint to confirm changing of status
-func queryStatusById(c *gin.Context) {
-    id := c.Param("id")
-    fmt.Print("id is: ", id)
-
-    fmt.Print("into the status endpoit")
-     msg := &types.MsgCreateDiscountTokenStatus{
-         Status: "unclaimed",
-         TokenID: 1,
-         Creator: addr,
-     }
-
-    // to create a post request to update status ..store response in txResp
-    txResp, transerr := cosmos.BroadcastTx(account, msg)
-    if transerr != nil {
-        fmt.Print("oops!")
-        log.Fatal(transerr)
-    }
-
-    // Print response from broadcasting a transaction
-    fmt.Print("Changed status of token id 1:\n\n")
-    fmt.Println(txResp)
-
-	c.IndentedJSON(http.StatusOK, txResp)
-}
-
-
-func createDiscountMembershipToken(c *gin.Context) {
-    // msg := &types.MsgCreateDiscountToken{
+func createMembershipToken(c *gin.Context) {
+    
     msg := &types.MsgCreateMembershipToken{
-        Creator: addr,
-	 	Timestamp: "timestamp",
+        Creator:           addr,
+	 	Timestamp:         "2 NOv 2022",
         ActivityName:      "Weekly leaderboard",
         Score:             "10",
         Message:           "Impresionante",
-        MembershipDuration:     "3",
+        MembershipDuration:"3",
+		EligibleCompanies: "Building Block Fitness",
         ExpiryDate:        "5th Dec 2022",
     }
-    // Broadcast a transaction from account `alice` with the message
-    // to create a post store response in txResp
+
     txResp, transerr := cosmos.BroadcastTx(account, msg)
     if transerr != nil {
         log.Fatal(transerr)
     }
 
-    // Print response from broadcasting a transaction
-    fmt.Print("MsgCreateMemberToken:\n\n")
+    fmt.Print("MsgCreateMembershipToken:\n\n")
     fmt.Println(txResp)
+
+	msgStatus := &types.MsgCreateMembershipTokenStatus{
+        Creator:           addr,
+		TokenID:           0,
+		Timestamp:         "2 Nov 2022",
+		Status:            "Valid",
+    }
+
+    txRespStatus, transerrStatus := cosmos.BroadcastTx(account, msgStatus)
+    if transerrStatus != nil {
+        log.Fatal(transerrStatus)
+    }
+
+    fmt.Print("MsgCreateMembershipTokenStatus:\n\n")
+    fmt.Println(txRespStatus)
 
 	c.IndentedJSON(http.StatusOK, txResp)
 
 }
 
-
-
-// func createDiscountToken(c *gin.Context) {
-
-func createDiscountBurritoToken(c *gin.Context) {
-
-        // Define a message to create a discount token
-    // msg := &types.MsgCreateDiscountToken{
-    //     Creator: addr,
-	// 	Timestamp: "timestamp",
-	// 	ActivityName:      "ActivityName",
-	// 	Score:             "Score",
-	// 	Message:           "Message",
-	// 	DiscountValue:     "DiscountValue",
-	// 	EligibleCompanies: "EligibleCompanies",
-	// 	ItemType:          "ItemType",
-	// 	ExpiryDate:        "ExpiryDate",
-    // }
-        // Define a message to create a discount token
+func createDiscountToken(c *gin.Context) {
+    
+    // Define a message to create a discount token
     msg := &types.MsgCreateDiscountToken{
-        Creator: addr,
-	 	Timestamp: "timestamp",
+        Creator:           addr,
+	 	Timestamp:         "2 Nov 2022",
         ActivityName:      "Learn to make tacos",
         Score:             "10",
         Message:           "Excelente",
         DiscountValue:     "5",
+		EligibleCompanies: "Cooking Academy",
         ItemType:          "protein burrito cooking class",
         ExpiryDate:        "5th Dec 2022",
     }
 
-    // Broadcast a transaction from account `alice` with the message
-    // to create a post store response in txResp
     txResp, transerr := cosmos.BroadcastTx(account, msg)
     if transerr != nil {
         log.Fatal(transerr)
@@ -194,32 +121,100 @@ func createDiscountBurritoToken(c *gin.Context) {
     fmt.Print("MsgCreateDiscountToken:\n\n")
     fmt.Println(txResp)
 
+	msgStatus := &types.MsgCreateDiscountTokenStatus{
+        Creator:           addr,
+		TokenID:           0,
+		Timestamp:         "2 Nov 2022",
+		Status:            "Valid",
+    }
+
+    txRespStatus, transerrStatus := cosmos.BroadcastTx(account, msgStatus)
+    if transerrStatus != nil {
+        log.Fatal(transerrStatus)
+    }
+
+    fmt.Print("MsgCreateDiscountTokenStatus:\n\n")
+    fmt.Println(txRespStatus)
+
 	c.IndentedJSON(http.StatusOK, txResp)
 }
 
+func deleteDiscountTokenStatus(c *gin.Context) {
+
+	msgStatus := &types.MsgDeleteDiscountTokenStatus{
+        Creator:               addr,
+		DiscountTokenStatusID: 0,
+		TokenID:               0,
+    }
+
+    txRespStatus, transerrStatus := cosmos.BroadcastTx(account, msgStatus)
+    if transerrStatus != nil {
+        log.Fatal(transerrStatus)
+    }
+
+    fmt.Print("MsgDeleteDiscountTokenStatus:\n\n")
+    fmt.Println(txRespStatus)
+
+	msgNewStatus := &types.MsgCreateDiscountTokenStatus{
+        Creator:           addr,
+		TokenID:           0,
+		Timestamp:         "2 Nov 2022",
+		Status:            "Invalid",
+    }
+
+    txRespNewStatus, transerrNewStatus := cosmos.BroadcastTx(account, msgNewStatus)
+    if transerrNewStatus != nil {
+        log.Fatal(transerrStatus)
+    }
+
+    fmt.Print("MsgCreateDiscountTokenStatus:\n\n")
+    fmt.Println(txRespNewStatus)
+
+	c.IndentedJSON(http.StatusOK, txRespStatus)
+}
+
+func deleteMembershipTokenStatus(c *gin.Context) {
+
+	msgStatus := &types.MsgDeleteMembershipTokenStatus{
+        Creator:               addr,
+		MembershipTokenStatusID: 0,
+		TokenID:                 0,
+    }
+
+    txRespStatus, transerrStatus := cosmos.BroadcastTx(account, msgStatus)
+    if transerrStatus != nil {
+        log.Fatal(transerrStatus)
+    }
+
+    fmt.Print("MsgDeleteMembershipTokenStatus:\n\n")
+    fmt.Println(txRespStatus)
+
+	msgNewStatus := &types.MsgCreateMembershipTokenStatus{
+        Creator:           addr,
+		TokenID:           0,
+		Timestamp:         "2 Nov 2022",
+		Status:            "Invalid",
+    }
+
+    txRespNewStatus, transerrNewStatus := cosmos.BroadcastTx(account, msgNewStatus)
+    if transerrNewStatus != nil {
+        log.Fatal(transerrStatus)
+    }
+
+    fmt.Print("MsgCreateMembershipTokenStatus:\n\n")
+    fmt.Println(txRespNewStatus)
+
+
+	c.IndentedJSON(http.StatusOK, txRespStatus)
+}
+
 func main() {
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    if accounterr != nil {
-        log.Fatal(accounterr)
-    }
-
-    if addresserr != nil {
-        log.Fatal(addresserr)
-    }
-
     router := gin.Default()
     router.Use(CORSMiddleware())
 
-	router.GET("/books", getBooks)
-    // router.GET("/token", createDiscountToken)
-    router.GET("/tokens", getTokens)
-    router.GET("/user", getUser)
-    router.GET("/discountBurritoToken", createDiscountBurritoToken)
-    router.GET("/discountMembershipToken", createDiscountMembershipToken)
-    router.GET("/status/:id", queryStatusById)
+    router.GET("/discountToken", createDiscountToken)
+    router.GET("/membershipToken", createMembershipToken)
+    router.GET("/deleteDiscountTokenStatus", deleteDiscountTokenStatus)
+	router.GET("/deleteMembershipTokenStatus", deleteMembershipTokenStatus)
 	router.Run("localhost:8080")
-
 }
